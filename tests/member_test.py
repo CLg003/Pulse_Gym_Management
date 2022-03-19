626f76379452bdedd7a407df4570884f2fec49bf
CLg003 <clairemjlaing@gmail.com>
2022-03-19 14:31:36 +0000

    Member class tests passed

--- tests/member_test.py
+++ tests/member_test.py
@@ -6,4 +6,14 @@ class TestMember(unittest.TestCase):
     def setUp(self):
         self.member_1 = Member("Sarah", "Kent", "698 Candlewood Lane, Cabot Cove", "sarah.kent@email.com")
 
-    
+    def test_member_has_first_name(self):
+        self.assertEqual("Sarah", self.member_1.first_name)
+
+    def test_member_has_last_name(self):
+        self.assertEqual("Kent", self.member_1.last_name)
+
+    def test_member_has_address(self):
+        self.assertEqual("698 Candlewood Lane, Cabot Cove", self.member_1.address)
+
+    def test_member_has_email(self):
+        self.assertEqual("sarah.kent@email.com", self.member_1.email)
