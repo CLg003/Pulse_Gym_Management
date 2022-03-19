1fe080910049af49f0a60c82c1cd94cea303f0fd
CLg003 <clairemjlaing@gmail.com>
2022-03-19 14:34:43 +0000

    FitnessClass class tests passed

--- tests/fitness_class_test.py
+++ tests/fitness_class_test.py
@@ -5,3 +5,15 @@ class TestFitnessClass(unittest.TestCase):
 
     def setUp(self):
         self.class_1 = FitnessClass("Yoga", "Mind & Body", "Monday", "13:00")
+
+    def test_class_has_name(self):
+        self.assertEqual("Yoga", self.class_1.name)
+
+    def test_class_has_category(self):
+        self.assertEqual("Mind & Body", self.class_1.category)
+
+    def test_class_has_day(self):
+        self.assertEqual("Monday", self.class_1.day)
+
+    def test_class_has_time(self):
+        self.assertEqual("13:00", self.class_1.time)
